cf844b76d490ce5cb4deac6845edc1bd424bbab8
xonx4l <sk797119@gmail.com>
2025-10-13 15:56:54 +0530

    add logo

--- src/app/_components/AppSidebar.tsx
+++ src/app/_components/AppSidebar.tsx
@@ -6,12 +6,14 @@ import {
     SidebarGroup,
     SidebarHeader,
   } from "@/components/ui/sidebar"
+import Image from 'next/image';
 
 function AppSidebar() {
     return (
         <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
+      <SidebarHeader className='bg-accent' />
+      <Image src={'/complexlogofix.png'} alt='logo' width={260} height={20} />
+      <SidebarContent className='bg-accent'>
         <SidebarGroup />
         <SidebarGroup />
       </SidebarContent>
