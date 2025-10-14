c001c424c160bfd1df6782ac3b2cdff0c69c5f76
xonx4l <sk797119@gmail.com>
2025-10-14 23:52:00 +0530

    add menu_options

--- src/app/_components/AppSidebar.tsx
+++ src/app/_components/AppSidebar.tsx
@@ -7,11 +7,34 @@ import {
     SidebarHeader,
   } from "@/components/ui/sidebar"
 import Image from 'next/image';
+import { Compass, GalleryHorizontalEnd, LogIn, Search } from 'lucide-react';
 
+const MenuOptions=[
+  {
+    title: 'Home',
+    icon: Search,
+    path: '/'
+  },
+  {
+    title: 'Discover',
+    icon: Compass,
+    path: '/'
+  },
+  {
+    title: 'Home',
+    icon: GalleryHorizontalEnd,
+    path: '/'
+  },
+  {
+    title: 'Home',
+    icon: LogIn,
+    path: '/'
+  },
+]
 function AppSidebar() {
     return (
         <Sidebar>
-      <SidebarHeader className='bg-accent' />
+      <SidebarHeader className='bg-accent flex items-center py-5'/>
       <Image src={'/complexlogofix.png'} alt='logo' width={260} height={20} />
       <SidebarContent className='bg-accent'>
         <SidebarGroup />
