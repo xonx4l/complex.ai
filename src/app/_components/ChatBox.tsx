af9fa8d8d769232146e7238e5311489b4701e3c0
xonx4l <sk797119@gmail.com>
2025-10-18 23:22:59 +0530

    add tabs

--- src/app/_components/ChatBox.tsx
+++ src/app/_components/ChatBox.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 function ChatBox() {
   return (
     <div className='flex flex-col h-screen items-center justify-center w-full'>
           //image
-    <div>
-      <input type='text' placeholder='Ask Anything' className='w-full' />
+    <div className='p-5 w-full  max-w-2xl border rounded-2xl'>
+      <input type='text' placeholder='Ask Anything' className='w-full  p-4 outline-none' />
+
+      <div>
+      <Tabs defaultValue="account" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="account">Account</TabsTrigger>
+    <TabsTrigger value="password">Password</TabsTrigger>
+  </TabsList>
+  <TabsContent value="account">Make changes to your account here.</TabsContent>
+  <TabsContent value="password">Change your password here.</TabsContent>
+</Tabs>
+      </div>
     </div>
     </div>
   )
